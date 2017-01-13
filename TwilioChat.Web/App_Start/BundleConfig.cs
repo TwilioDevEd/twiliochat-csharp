2018d485ce96d1e14abee712b0acbf64fb403a97
Hector Ortega <hector.oj@gmail.com>
2017-01-13 12:39:13 -0500

    Add JS tests and npm/karma configuration

--- TwilioChat.Web/App_Start/BundleConfig.cs
+++ TwilioChat.Web/App_Start/BundleConfig.cs
@@ -8,25 +8,25 @@ namespace TwilioChat.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/lib/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/lib/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/lib/bootstrap.js",
+                      "~/Scripts/lib/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/twiliochat").Include(
-                      "~/Scripts/jquery-throttle.min.js",
-                      "~/Scripts/jquery.loadTemplate-1.4.4.min.js",
-                      "~/Scripts/twiliochat.js",
-                      "~/Scripts/dateformatter.js"));
+                      "~/Scripts/lib/jquery-throttle.min.js",
+                      "~/Scripts/lib/jquery.loadTemplate-1.4.4.min.js",
+                      "~/Scripts/lib/twiliochat.js",
+                      "~/Scripts/lib/dateformatter.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
